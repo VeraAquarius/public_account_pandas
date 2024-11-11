a68e74a062bce14dfeff712fec81c39b4bd8cc27
VeraAquarius <15821886267@163.com>
2024-11-11 21:57:10 +0800

    string join 将列表中的字符串连接成一个单一的字符串

---
+++ ahuang/wang/pandas/stringJoin.py
@@ -0,0 +1,17 @@
+# 定义一个字符串列表
+words = ["Hello", "world", "this", "is", "Python"]
+
+# 使用' '.join()方法将列表中的字符串连接成一个单一的字符串
+sentence = ' '.join(words)
+
+# 显示连接后的结果
+print("Joined String:", sentence)
+
+# 定义一个元组，包含不同格式的日期部分
+date_parts = ("2024", "05", "19")
+
+# 使用'-'.join()方法将元组中的字符串连接成一个日期字符串
+date_string = '-'.join(date_parts)
+
+# 显示连接后的日期字符串
+print("Joined Date String:", date_string)
