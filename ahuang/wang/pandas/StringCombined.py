64c94950aa4a158ee424e7d286e1c1b5603f071e
VeraAquarius <15821886267@163.com>
2024-11-11 21:47:43 +0800

    string Combined 合并字符串

---
+++ ahuang/wang/pandas/StringCombined.py
@@ -0,0 +1,20 @@
+# 定义两个字符串
+str1 = "Hello, "
+str2 = "world!"
+
+# 使用'+'操作符合并字符串
+combined_str = str1 + str2
+
+# 显示合并后的结果
+print("Combined String:", combined_str)
+
+# 定义多个字符串
+str3 = "Python "
+str4 = "is "
+str5 = "awesome!"
+
+# 使用'+'操作符合并多个字符串
+combined_str_multiple = str3 + str4 + str5
+
+# 显示合并多个字符串后的结果
+print("Combined String (Multiple):", combined_str_multiple)
