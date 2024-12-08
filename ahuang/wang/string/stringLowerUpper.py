09b80a5233bf0ce99e1a00630e7987288d467188
VeraAquarius <15821886267@163.com>
2024-12-08 15:21:11 +0800

    string 大小写转换

---
+++ ahuang/wang/string/stringLowerUpper.py
@@ -0,0 +1,14 @@
+# 定义一个字符串
+text = "Hello, World! This is a Test String."
+
+# 使用lower方法将字符串转换为小写
+lower_case = text.lower()
+
+# 显示转换为小写后的结果
+print("Lower Case:", lower_case)
+
+# 使用upper方法将字符串转换为大写
+upper_case = text.upper()
+
+# 显示转换为大写后的结果
+print("Upper Case:", upper_case)
