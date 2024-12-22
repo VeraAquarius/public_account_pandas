52aac0a59c456397638575dabe42ee4a8c36eada
VeraAquarius <15821886267@163.com>
2024-12-22 15:09:26 +0800

    matplotlib  pylab模式下 执行figsize 大小创建图形

--- ahuang/wang/matplotlib/matplotlibFigure.py
+++ ahuang/wang/matplotlib/matplotlibFigure.py
@@ -5,24 +5,10 @@ import numpy as np
 x = np.linspace(0, 10, 100)
 y = np.sin(x)
 
-# 使用pylab模式绘图
+# 使用pylab模式绘图，并设置figsize参数
+plt.figure(figsize=(10, 6))  # 设置图形尺寸为10x6英寸
 plt.plot(x, y)
-plt.title('Pylab Mode Plot')
+plt.title('Plot with figsize')
 plt.xlabel('x')
 plt.ylabel('sin(x)')
-plt.show()
-
-# 使用Figure和subplot创建图形
-fig, ax = plt.subplots(2, 1)  # 创建一个包含2个子图的Figure
-
-# 在第一个子图上绘图
-ax[0].plot(x, y)
-ax[0].set_title('Subplot 1')
-
-# 在第二个子图上绘图
-ax[1].plot(x, np.cos(x))
-ax[1].set_title('Subplot 2')
-
-# 调整子图间距
-plt.tight_layout()
 plt.show()
